1b3b83445579deffc7b8ea046e922173efd8f19b
ZachHung <zachhung911@gmail.com>
2021-06-16 18:03:02 +0700

    Initialize

---
+++ example_mutex.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <pthread.h>
+int products=0;
+pthread_mutex_t mutex;
+void* procA(void* message){
+    while(1){
+		pthread_mutex_lock(&mutex);
+		if(products>0) 
+			products--;
+        printf("SELLS | products = %d\n",products);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+void* procB(void* message){
+    while(1){
+		pthread_mutex_lock(&mutex);
+        products++;
+        printf("PRODUCTS | products =  %d\n", products);
+        pthread_mutex_unlock(&mutex);
+    }
+}
+int main(){
+    pthread_mutex_init(&mutex, NULL);
+    pthread_t pA, pB;
+    pthread_create(&pA,NULL,&procA,NULL);
+    pthread_create(&pB, NULL, &procB, NULL);
+    while(1){}
+}
