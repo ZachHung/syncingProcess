e8baf2d0e73647ca9a5bdecf6dbf3eac2f74622f
ZachHung <zachhung911@gmail.com>
2021-06-18 12:57:44 +0700

    added

---
+++ Cau1.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+int sell=0;
+int product=0;
+sem_t sem;
+void* procA(void* message){
+    while(1){
+        sem_wait(&sem);
+        sell++;
+        printf("SELLS = %d\n",sell);
+    }
+}
+void* procB(void* message){
+    while(1){
+        product++;
+        printf("PRODUCTS= %d\n", product);
+        sem_post(&sem);
+    }
+}
+int main(){
+    sem_init(&sem,0,0);
+    pthread_t pA, pB;
+    pthread_create(&pA,NULL,&procA,NULL);
+    pthread_create(&pB, NULL, &procB, NULL);
+    while(1){}
+}
