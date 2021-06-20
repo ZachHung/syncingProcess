dbcbdc965e63831398a32b8f6560eb0ec29bba1d
ZachHung <zachhung911@gmail.com>
2021-06-20 09:22:19 +0700

    changed

--- Cau3.c
+++ Cau3.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <pthread.h>
 int x= 0;
+pthread_mutex_t mutex;
 void* procA(void* mess){
     while(1){
+        pthread_mutex_lock(&mutex);
         x=x+1;
         if(x==20)
             x=0;
         printf("A | %d\n",x);
+        pthread_mutex_unlock(&mutex);
     }
 }
 void* procB(void* mess){
     while(1){
+        pthread_mutex_lock(&mutex);
         x=x+1;
         if (x==20)
             x=0;
         printf("B | %d\n",x);
+        pthread_mutex_unlock(&mutex);
     }
 }
 int main(){
